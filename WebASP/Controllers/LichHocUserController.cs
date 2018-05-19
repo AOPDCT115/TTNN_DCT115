3af3f64c005500087cceb159c3fdd7f5c6b79bcf
DESKTOP-RIAC65G\travo <travo9692@gmail.com>
2018-05-20 06:43:55 +0700

    tra commit 20/05

---
+++ WebASP/Controllers/LichHocUserController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebASP.Common;
+using WebASP.DAL;
+using WebASP.Models;
+
+namespace WebASP.Controllers
+{
+    public class LichHocUserController : Controller
+    {
+        private DataVTT db = new DataVTT();
+        
+
+        public ActionResult XemmThoiKhoaBieu()
+        {
+            var std = (UserLogin)Session[CommonConstants.USER_SESSION];
+            if (std != null)
+            {
+                var id = std.UserID;
+
+                var schedule = from a in db.Register
+                               join b in db.Class on a.ClassID equals b.ClassID
+                               join c in db.Schedule on b.ClassID equals c.ClassID
+                               where a.StudentID == id
+                               select c;
+
+
+                var model = schedule.ToList();
+                ViewBag.schedule = schedule.ToList();
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index","LichHoc"); 
+            }
+        }
+    }
+}
+            
+
+           
+
+    
